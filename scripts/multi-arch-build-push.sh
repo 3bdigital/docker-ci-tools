c4d02d7bb6defc8d79b0a9ee8be7d6f0653ed7b1
David Matthews <david@3bweb.com>
2023-01-05 17:14:15 +0000

    Fixed tagging in build script

--- scripts/multi-arch-build-push.sh
+++ scripts/multi-arch-build-push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Build and push to Docker Hub
-docker buildx build --push --platform linux/amd64,linux/arm64 -t 3bdigital/ci-tools:alpine 3bdigital/ci-tools:latest alpine
+docker buildx build --push --platform linux/amd64,linux/arm64 -t 3bdigital/ci-tools:alpine -t 3bdigital/ci-tools:latest alpine
 docker buildx build --push --platform linux/amd64,linux/arm64 -t 3bdigital/ci-tools:ubuntu ubuntu
